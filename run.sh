ec7997e81ba0be546145c859124b47bf1cb6f858
Michael Mokrysz <hi@46bit.com>
2016-11-12 00:04:20 +0000

    Political Compass for #cs-york is now included in cronjob.

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+pypy3 political_compass.py \
+  -i ../znc-logs/freenode/\#cs-york \
+  -d ../znc-logs/freenode/dedupe.txt \
+  -o webroot/cs-york-pc-political-compass.json
+
+
 log_directory=${argv[1]}
 print "Running in log directory:\n  ${log_directory}"
 for channel in "${log_directory}"/*
@@ -17,5 +23,3 @@ do
   cp "${player_message_counts}" "webroot/${channelname}.json"
   cp "webroot/_channel_info.html" "webroot/${channelname}.html"
 done
-
-#python3 political_compass.py -i ../znc-logs/freenode/\#cs-york -d ../znc-logs/freenode/dedupe.txt -o webroot/cs-york-pc-political-compass.json
